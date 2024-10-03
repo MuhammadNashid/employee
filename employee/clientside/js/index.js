8fe9b9e5fd4bccf734fedabf5140238259d8c4e5
MuhammadNashid <muhammadnashid905@gmail.com>
2024-10-03 11:35:19 +0530

    updated

---
+++ employee/clientside/js/index.js
@@ -0,0 +1,33 @@
+async function employees(params) {
+    const res=await fetch("http://localhost:3000/employees")
+   const data=await res.json()
+   console.log(data);
+   
+   let str=``
+   data.map((datas)=>{
+    str+=`
+      <div class="sub1">
+    <input class="txt" type="text" id="name-${datas._id}" placeholder="Employee id" value="${datas.EID}">
+    <input class="txt" type="text" id="name-${datas._id}" placeholder="Name" value="${datas.NAME}">
+    <input class="txt" type="text" id="name-${datas._id}" placeholder="Designation" value="${datas.DES}">
+    <input class="txt" type="text" id="name-${datas._id}" placeholder="Phone Number" value="${datas.PHONE}">
+    <input class="txt" type="text" id="name-${datas._id}" placeholder="Email" value="${datas.EMAIL}">
+    <input class="txt" type="text" id="name-${datas._id}" placeholder="Salary" value="${datas.SALARY}">
+    <input class="txt" type="text" id="name-${datas._id}" placeholder="Details" value="${datas.DETAILS}">
+    <button class="btn" type="button">Add Employee</button>
+</div>  
+    
+
+    `
+   })
+   document.getElementById("display").innerHTML=str
+    
+}
+employees()
+ function handleEdit(id){
+    document.getElementById(`name-${id}`).disabled=false
+    document.getElementById(`email-${id}`).disabled=false
+    document.getElementById(`phone-${id}`).disabled=false
+    document.getElementById(`blood-${id}`).disabled=false
+    document.getElementById(`gender-${id}`).disabled=false
+ }
